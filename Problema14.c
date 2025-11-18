0a659948ceedb3539562a02b5b3d3a09b80221bc
Seiji314288758 <nietoseiji@gmail.com>
2025-11-18 00:52:41 -0600

    Add array element counting program
    Implement a program to count occurrences of a number in an array.

---
+++ Problema14.c
@@ -0,0 +1,36 @@
+/*Victor Seiji Nieto Morita
+Tarea 2 Problema Arreglos 2*/
+
+#include <stdio.h>
+
+int main ()
+{
+  int n, numero, contador = 0;
+
+  printf("Ingrese la cantidad de elementods del arreglo: ");
+  scanf("%d", &n);
+
+  int arreglo[n];
+
+  for (int i = 0; i < n; i++)
+      {
+       printf("Ingrese el elemento %d: ", i + 1);
+       scanf("%d", &arreglo[i]);
+      }
+
+  printf("\nIngrese el numero que desea buscar: ");
+  scanf("%d", &numero);
+
+  for (int i = 0; i < n; i++)
+     {
+      if (arreglo[i] == numero)
+         {
+          contador++;
+         }
+     }
+
+ printf("\nEl numero %d se encuentra %d veces en el arreglo.\n", numero, contador);
+
+return 0;
+
+}
