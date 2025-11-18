f788212592ad041f31900e5b9244c09dfcbec504
Seiji314288758 <nietoseiji@gmail.com>
2025-11-18 00:01:47 -0600

    Implement number categorization in Problema10.c
    Count and categorize 100 natural numbers input by the user.

--- Problema10.c
+++ Problema10.c
@@ -5,4 +5,36 @@ Tarea 2 Problema Estructuras Repetitivas 2*/
 
 int main ()
 {
-  
+  int num;
+  int positivos = 0, negativos = 0, nulos = 0;
+  int i;
+
+  printf("Ingrese 100 numero naturales (pueden incluir el 0): ");
+
+  for (i = 1; i <= 100; i++)
+    {
+     printf("Numero %d: ", i);
+     scanf("%d", &num);
+
+      if (num > 0)
+       {
+        positivos++;
+       }
+      else if (num < 0)
+       {
+        negativos++;
+       }
+      else 
+       {
+        nulos++;
+       }
+    }
+
+  printf("\nResultados:\n");
+  printf("Positivos: %d\n", positivos);
+  printf("Negativos: %d\n", negativos);
+  printf("Nulos (0): %d\n", nulos);
+
+  return 0;
+}
+    
