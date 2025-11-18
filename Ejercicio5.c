45f6372f5d22bcde051f84b599a212d043898d1e
Seiji314288758 <nietoseiji@gmail.com>
2025-11-17 19:00:35 -0600

    Add Ejercicio5.c for purchase calculation

---
+++ Ejercicio5.c
@@ -0,0 +1,26 @@
+/*Victor Seiji Nieto Morita
+Tarea 2 Problema If 1*/
+
+#include <stdio.h>
+
+int main ()
+{
+  float compra, total;
+
+  printf("Ingrese el monto de la compra: ");
+  scanf("%f", &compra);
+
+  if (compra < 2500)
+   {
+    total = compra - (compra * 0.08);
+   }
+
+  else
+  {
+   total = compra;
+  }
+
+ printf("El total a pagar es: $%.2f\n", total);
+
+ return 0;
+}
